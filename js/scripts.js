36a1502cc4a7b0c37c1c7a3868db653c9f50b9da
Malin Nemergut <mmheineking@gmail.com>
2016-12-07 14:25:26 -0500

    fixed scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(function() {
-  $('a[href="#about"]').click(function(e){
+  $('a[href="#portfolio"]').click(function(e){
     e.preventDefault();
-    var targetOffset = $('a[name="about"]').offset().top;
+    var targetOffset = $('a[name="portfolio"]').offset().top;
     $('body').animate(
       {scrollTop: targetOffset},
       1000
