ff5a1047ba3efd135eea78aba22e46fe5d063eef
Malin Nemergut <mmheineking@gmail.com>
2016-12-13 23:07:32 -0500

    fixed formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,32 @@
-$(function() {
-  $('a[href="#portfolio"]').click(function(e){
-    e.preventDefault();
-    var targetOffset = $('a[name="portfolio"]').offset().top;
-    $('body').animate(
-      {scrollTop: targetOffset},
-      1000
-    );
+$(document).ready(function(){
+  $(function() {
+    $('a[href="#portfolio"]').click(function(e){
+      e.preventDefault();
+      var targetOffset = $('a[name="portfolio"]').offset().top;
+      $('body').animate(
+        {scrollTop: targetOffset},
+        1000
+      );
+    });
   });
-});
-$(function() {
-  $('a[href="#experience"]').click(function(e){
-    e.preventDefault();
-    var targetOffset = $('a[name="experience"]').offset().top;
-    $('body').animate(
-      {scrollTop: targetOffset},
-      1500
-    );
+  $(function() {
+    $('a[href="#experience"]').click(function(e){
+      e.preventDefault();
+      var targetOffset = $('a[name="experience"]').offset().top;
+      $('body').animate(
+        {scrollTop: targetOffset},
+        1500
+      );
+    });
   });
-});
-$(function() {
-  $('a[href="#contact"]').click(function(e){
-    e.preventDefault();
-    var targetOffset = $('a[name="contact"]').offset().top;
-    $('body').animate(
-      {scrollTop: targetOffset},
-      2000
-    );
+  $(function() {
+    $('a[href="#contact"]').click(function(e){
+      e.preventDefault();
+      var targetOffset = $('a[name="contact"]').offset().top;
+      $('body').animate(
+        {scrollTop: targetOffset},
+        2000
+      );
+    });
   });
 });
